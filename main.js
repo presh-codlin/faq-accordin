eeae51777071e3d239a6a1133c04498e4d8a9057
Yisrael Abraham <119142775+presh-codlin@users.noreply.github.com>
2024-02-07 23:22:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,17 +4,11 @@ const showHides = document.querySelectorAll(".show");
 function toggAns(id) {
   const targetItem = event.currentTarget.parentElement;
   showHides.forEach((showHide)=>{
-    if(showHide.src !== "./assets/images/icon-plus.svg"){
-      showHide.src = "./assets/images/icon-plus.svg";
-    }
+    showHide.src = "./assets/icon-plus.svg";
   })
   answers.forEach((answer)=>{
     answer.classList.remove("active");
   })
-  
   targetItem.parentElement.classList.toggle("active");
-  //console.log(event.currentTarget.src.value);
-  if(event.currentTarget.src !== "./assets/images/icon-minus.svg"){
-    event.currentTarget.src = "./assets/images/icon-minus.svg";
-  }
+  event.currentTarget.src = "./assets/icon-minus.svg";
 }
